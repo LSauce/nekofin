231cb743ab53c8ce9bd67f536d25d9dd1560c165
lonzzi <lonzzi@qq.com>
2025-09-02 18:05:21 +0800

    fix: add cancel button text to SearchScreen component

--- app/(tabs)/(search)/index.tsx
+++ app/(tabs)/(search)/index.tsx
@@ -111,6 +111,7 @@ export default function SearchScreen() {
               setSelected('');
             },
             hideWhenScrolling: false,
+            cancelButtonText: '取消',
           },
         }}
       />
