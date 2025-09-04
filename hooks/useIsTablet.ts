9cdbebcdedfe8756e0268610b196ded8bb5aa510
lonzzi <lonzzi@qq.com>
2025-09-04 20:00:07 +0800

    refactor: update useIsTablet to include desktop device type

--- hooks/useIsTablet.ts
+++ hooks/useIsTablet.ts
@@ -3,6 +3,9 @@ import { useMemo } from 'react';
 
 export function useIsTablet() {
   return useMemo(() => {
-    return Device.deviceType === Device.DeviceType.TABLET;
+    return (
+      Device.deviceType === Device.DeviceType.TABLET ||
+      Device.deviceType === Device.DeviceType.DESKTOP
+    );
   }, []);
 }
