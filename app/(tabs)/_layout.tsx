0fd5f9610ea12900799c45573ad50ebf0d00293d
lonzzi <lonzzi@qq.com>
2025-09-13 17:44:44 +0800

    chore: use  expo-router NativeTabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,61 +1,62 @@
-import { Tabs } from '@/components/BottomTabs';
 import { useAccentColor } from '@/lib/contexts/ThemeColorContext';
+import { Icon, NativeTabs } from 'expo-router/unstable-native-tabs';
 import { Platform } from 'react-native';
 
 export default function TabLayout() {
   const { accentColor } = useAccentColor();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: accentColor,
-      }}
-      initialRouteName="(home)"
-    >
-      <Tabs.Screen redirect name="index" />
-      <Tabs.Screen
+    <NativeTabs minimizeBehavior="onScrollDown">
+      <NativeTabs.Trigger name="index" hidden />
+      <NativeTabs.Trigger
         name="(home)"
         options={{
           title: '首页',
-          tabBarIcon:
-            Platform.OS === 'android'
-              ? () => require('@/assets/icons/film.png')
-              : ({ focused }) => (focused ? { sfSymbol: 'film.fill' } : { sfSymbol: 'film' }),
         }}
-      />
-      <Tabs.Screen
+      >
+        <Icon
+          {...(Platform.OS === 'android' && { src: require('@/assets/icons/film.png') })}
+          sf={{ default: 'film', selected: 'film.fill' }}
+          selectedColor={accentColor}
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger
         name="(search)"
         options={{
           title: '搜索',
-          tabBarIcon:
-            Platform.OS === 'android'
-              ? () => require('@/assets/icons/search.png')
-              : ({ focused }) =>
-                  focused ? { sfSymbol: 'magnifyingglass' } : { sfSymbol: 'magnifyingglass' },
           role: 'search',
         }}
-      />
-      <Tabs.Screen
+      >
+        <Icon
+          {...(Platform.OS === 'android' && { src: require('@/assets/icons/search.png') })}
+          sf={{ default: 'magnifyingglass', selected: 'magnifyingglass' }}
+          selectedColor={accentColor}
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger
         name="(favorites)"
         options={{
           title: '收藏',
-          tabBarIcon:
-            Platform.OS === 'android'
-              ? () => require('@/assets/icons/heart.png')
-              : ({ focused }) => (focused ? { sfSymbol: 'heart.fill' } : { sfSymbol: 'heart' }),
         }}
-      />
-      <Tabs.Screen
+      >
+        <Icon
+          {...(Platform.OS === 'android' && { src: require('@/assets/icons/heart.png') })}
+          sf={{ default: 'heart', selected: 'heart.fill' }}
+          selectedColor={accentColor}
+        />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger
         name="(settings)"
         options={{
           title: '设置',
-          tabBarIcon:
-            Platform.OS === 'android'
-              ? () => require('@/assets/icons/settings.png')
-              : ({ focused }) =>
-                  focused ? { sfSymbol: 'gearshape.fill' } : { sfSymbol: 'gearshape' },
         }}
-      />
-    </Tabs>
+      >
+        <Icon
+          {...(Platform.OS === 'android' && { src: require('@/assets/icons/settings.png') })}
+          sf={{ default: 'gearshape', selected: 'gearshape.fill' }}
+          selectedColor={accentColor}
+        />
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,5 @@
+import { Redirect } from 'expo-router';
+
 export default function Tabs() {
-  return null;
+  return <Redirect href="/(tabs)/(home)" />;
 }
